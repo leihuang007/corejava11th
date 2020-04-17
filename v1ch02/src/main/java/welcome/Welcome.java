b31f73bf8963e6284919693c460d1bd80d8fa171
leihuang007 <leihuang007@qq.com>
2020-04-17 11:50:26 +0800

    v1ch02 Welcome例子

---
+++ v1ch02/src/main/java/welcome/Welcome.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package welcome;
+
+/**
+ * @author leihuang
+ *
+ */
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String greeting = "Welcome to Core Java";
+		System.out.println(greeting);
+		for (int i = 0; i < greeting.length(); i++) {
+			System.out.print("=");
+		}
+		System.out.println();
+	}
+
+}
