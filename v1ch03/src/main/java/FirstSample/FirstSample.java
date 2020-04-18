fd66133ad61ecda6939a6e0efa5baa85caff8040
leihuang007 <leihuang007@qq.com>
2020-04-18 20:29:03 +0800

    v1ch03 FirstSample

---
+++ v1ch03/src/main/java/FirstSample/FirstSample.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package FirstSample;
+
+/**
+ * @author Home
+ *
+ */
+public class FirstSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("We will not use 'Hello, World'");
+	}
+
+}
